9ce0200e428f227f7e9b30a85b8fefa33d2bfb0d
SamarthShrivas <SamarthShrivas@users.noreply.github.com>
2022-10-31 21:57:00 +0530

    commit

--- example/example.py
+++ example/example.py
@@ -1,10 +1,3 @@
 from utils import slowedreverb
 
-# Make sure ffmpeg and sox is install 
-
-# Linux : sudo apt install ffmpeg sox
-# Windows : 
-#       ffmpeg : https://ffmpeg.org/download.html
-#       sox : https://sourceforge.net/projects/sox/
-
 slowedreverb('saware.wav', 'lofi saware.wav')
